88b83ecfdc98692e0aea050f05a805fb3bb1f4c5
Bauke Molenaar <bauke.molenaar@gmail.com>
2021-08-02 10:08:15 +0200

    Update potfilesorter.py

--- potfilesorter.py
+++ potfilesorter.py
@@ -2,11 +2,20 @@ import sys, os, subprocess
 import urllib.request
 from shutil import copyfile
 
+potfile_source = '/home/rizzo/wpa-sec.founds.potfile'
+dlurl = 'https://wpa-sec.stanev.org/?api&dl=1'
+
 wpa_source = "/etc/wpa_supplicant/wpa_supplicant.conf"
 wpa_backup = "/tmp/wpa_supplicant.bak"
 wpa_tmp = "/tmp/wpa_supplicant.tmp"
-potfile_source = '/home/pi/wpa-sec.founds.potfile'
-dlurl = 'https://wpa-sec.stanev.org/?api&dl=1'
+
+wificonfigstore_source = "/home/rizzo/WiFiConfigStore.xml"
+wificonfigstore_backup = "/tmp/wificonfigstore.bak"
+wificonfigstore_tmp = "/tmp/wificonfigstore.tmp"
+
+wificonfigstoresoftap_source = "/home/rizzo/WiFiConfigStoreSoftAp.xml"
+wificonfigstoresoftap_backup = "/tmp/wificonfigstoresoftap.bak"
+wificonfigstoresoftap_tmp = "/tmp/wificonfigstoresoftap.tmp"
 
 def get_potfile():
     print("Download your potfile from: " + dlurl)
@@ -22,17 +31,50 @@ def backup_configs():
         copyfile(wpa_source, wpa_backup)
         print('Create tempfile to work with in: ' + wpa_tmp)
         copyfile(wpa_source, wpa_tmp)
-
+        
+    if os.path.exists(wificonfigstore_tmp):
+        os.remove(wificonfigstore_tmp)
+    else:
+        print('Backing up: ' + wificonfigstore_source)
+        print('To: ' + wpa_backup)
+        copyfile(wificonfigstore_source, wificonfigstore_backup)
+        print('Create tempfile to work with in: ' + wificonfigstore_tmp)
+        copyfile(wificonfigstore_source, wificonfigstore_tmp)
+        
+    if os.path.exists(wificonfigstoresoftap_tmp):
+        os.remove(wificonfigstoresoftap_tmp)
+    else:
+        print('Backing up: ' + wificonfigstoresoftap_source)
+        print('To: ' + wificonfigstoresoftap_backup)
+        copyfile(wificonfigstoresoftap_source, wificonfigstoresoftap_backup)
+        print('Create tempfile to work with in: ' + wificonfigstoresoftap_tmp)
+        copyfile(wificonfigstoresoftap_source, wificonfigstoresoftap_tmp)
+        
 def copy_config():
-    print('Copying new created config to: ' + wpa_source)
     if os.path.exists(wpa_tmp):
+        print('Copying new created config to: ' + wpa_source)
         copyfile(wpa_tmp, wpa_source)
         os.remove(wpa_tmp)
     else:
         print('Cannot copy: ' + wpa_tmp + ' to: ' + wpa_source)
         print('Are you ROOT?')
         exit()
-
+    
+    if os.path.exists(wificonfigstore_tmp):
+        print('Copying new created config to: ' + wificonfigstore_source)
+        copyfile(wificonfigstore_tmp, wificonfigstore_source)
+        os.remove(wificonfigstore_tmp)
+     else:
+        print('Cannot copy: ' + wpa_tmp + ' to: ' + wpa_source)
+        print('Are you ROOT?')
+        exit()
+    
+    if os.path.exists(wificonfigstoresoftap_tmp):
+        print('Copying new created config to: ' + wificonfigstore_source)
+        copyfile(wificonfigstoresoftap_tmp, wificonfigstoresoftap_source)
+        os.remove(wificonfigstoresoftap_tmp)
+        exit()
+        
 def checkwpaconfig(wpa_tmp, search_str):
     with open(wpa_tmp, 'r') as checklines:
         for line in checklines:
@@ -61,6 +103,28 @@ def readpotfiledata():
                 print(bssid + ' Found, Skipping.')
             else:
                 with open(wpa_tmp, 'a+') as outputfile:
+                    print('Found new network: ' + bssid)
+                    print('Appending to: ' + outputfile.name)
+                    outputfile.writelines('\n')
+                    outputfile.writelines('network={' + '\n')
+                    outputfile.writelines('  scan_ssid=1' + '\n')
+                    outputfile.writelines('  ssid="' + bssid + '"\n')
+                    outputfile.writelines('  psk="' + wpapassword + '"\n')
+                    outputfile.writelines('}\n')
+                    outputfile.writelines('\n')
+                    
+                with open(wificonfigstore_tmp, 'a+') as outputfile:
+                    print('Found new network: ' + bssid)
+                    print('Appending to: ' + outputfile.name)
+                    outputfile.writelines('\n')
+                    outputfile.writelines('network={' + '\n')
+                    outputfile.writelines('  scan_ssid=1' + '\n')
+                    outputfile.writelines('  ssid="' + bssid + '"\n')
+                    outputfile.writelines('  psk="' + wpapassword + '"\n')
+                    outputfile.writelines('}\n')
+                    outputfile.writelines('\n')
+                    
+                with open(wificonfigstoresoftap_tmp, 'a+') as outputfile:
                     print('Found new network: ' + bssid)
                     print('Appending to: ' + outputfile.name)
                     outputfile.writelines('\n')
